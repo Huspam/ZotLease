e4c0f22d84f32120103866344ce3f3a61caf8451
Jon Le <jonathanle2021@gmail.com>
2023-02-07 00:32:57 -0800

    Simplified Program

--- app/main.py
+++ app/main.py
@@ -17,8 +17,8 @@ def index():
     listings = get_data(connection, cursor)
     return render_template('index.html',listings=listings)
 
-@app.route("/forms/vdc", methods=['GET', 'POST'])
-def vdc():
+@app.route("/forms/enter", methods=['GET', 'POST'])
+def enter():
     communities = ['Arroyo Vista', 'Camino del Sol', 'Puerta del Sol', 'Plaza Verde', 'Plaza Verde II', 'Vista Del Campo', 'Vista Del Campo Norte']
     roomtypes = ['Single', 'Double', 'Quad']
     genders = ['Male', 'Female']
@@ -28,41 +28,25 @@ def vdc():
     enddates = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     #price
     #description
-    return render_template('vdc.html', communities=communities, roomtypes=roomtypes, genders=genders, leasetypes=leasetypes, startdates=startdates, enddates=enddates)
+    return render_template('listEntry.html', communities=communities, roomtypes=roomtypes, genders=genders, leasetypes=leasetypes, startdates=startdates, enddates=enddates)
 
-@app.route("/forms/vdcn", methods=['GET'])
-def vdcn():
-    return render_template('vdcn.html')
-
-@app.route("/forms/plaza", methods=['GET'])
-def plaza():
-    return render_template('plaza.html')
-
-@app.route("/forms/puerta", methods=['GET'])
-def puerta():
-    return render_template('puerta.html')
-
-@app.route("/forms/arroyo", methods=['GET'])
-def arroyo():
-    return render_template('arroyo.html')
-
-@app.route("/listings/yourlistings", methods=['GET'])
+@app.route("/listings/yourListings", methods=['GET'])
 def pList():
     return render_template('personalList.html')
 
-@app.route("/listings/savedlistings", methods=['GET'])
+@app.route("/listings/savedListings", methods=['GET'])
 def sList():
     return render_template('saveList.html')
 
 @app.route("/listings", methods=['GET'])
 def listing():
-    return render_template('otherlisting.html')
+    return render_template('otherList.html')
 
 @app.route("/forms/success", methods=['GET', 'POST'])
 def add_listing():
     listing = [request.form.get(key) for key in request.form.keys()]
     insert_data(connection, cursor, *listing)
-    return render_template('listingsuccess.html')
+    return render_template('listSuccess.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
